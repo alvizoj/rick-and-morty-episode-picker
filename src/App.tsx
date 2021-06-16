88e31fb44935597e5c71f123e7b6fd6757fcad53
Joseph <alvizoj@uci.edu>
2021-06-15 23:39:49 -0700

    created fetch request that pulls list of episodes from back-end and displays them

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [episodesArray, setEpisodesArray] = React.useState([]);
+
+  useEffect( () => {
+      fetch("http://localhost:8000/episodes")
+          .then((response) => response.json())
+          .then((data) => {setEpisodesArray(data); console.log(data)})
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {episodesArray.map((episode) => (
+              <p>{episode}</p>
+          )
+          )}
       </header>
     </div>
   );
