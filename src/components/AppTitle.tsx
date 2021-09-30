cdf849649e1ff32048b1901b645a7c8d0a42c7f8
Joseph <alvizoj@uci.edu>
2021-09-30 01:29:47 -0700

    Added AppTitle to homepage

---
+++ src/components/AppTitle.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../css/AppTitle.css';
+
+export default function AppTitle(): JSX.Element {
+    return (
+        <div className='page-title'>
+            <p>Rick and Morty Episode Picker</p>
+        </div>
+    );
+}
--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,28 +1,15 @@
-import React from 'react';
-import { IEpisode } from '../model/IEpisode';
-import EpisodeEntry from './EpisodeEntry';
+import React, { useEffect } from 'react';
 import episodesService from '../services/episodesService';
+import AppTitle from './AppTitle';
 
 function HomeScreen(): JSX.Element {
     const { loading, error, episodes } = episodesService.useEpisodes();
 
-    return (
-        <div className='App'>
-            {loading && <p>LOADING</p>}
-            {error && <p>ERROR</p>}
-            {episodes &&
-                episodes.map((episode: IEpisode) => (
-                    <EpisodeEntry
-                        key={episode.id}
-                        id={episode.id}
-                        description={episode.description}
-                        title={episode.title}
-                        seasonNumber={episode.season_number}
-                        episodeNumber={episode.episode_number}
-                    />
-                ))}
-        </div>
-    );
+    useEffect(() => {
+        document.title = 'Rick And Morty Blog'; //Page title
+    });
+
+    return <AppTitle />;
 }
 
 export default HomeScreen;
