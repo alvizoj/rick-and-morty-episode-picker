4b920f92d569be082b8b41ef1987656aa4fe4be7
Joseph <alvizoj@uci.edu>
2021-10-01 20:11:02 -0700

    Added strings constants for initial color scheme

---
+++ src/constants/HexColors.ts
@@ -0,0 +1,8 @@
+export const PORTALGREEN: string = '#8BCF21';
+export const RICKHAIRBLUE: string = '#83D2E4';
+export const CAFE: string = '#B28057';
+export const BROWN: string = '#926F44';
+export const MORTYYELLOW: string = '#FBFD72';
+export const NAVYBLUE: string = '#24325F';
+export const WARNINGRED: string = '#FB6467';
+export const TARTRED: string = '#FA383B';
