f4cfb032ac4d1305d679fc3b45027772cf1aac0a
Joseph <alvizoj@uci.edu>
2021-10-03 22:52:01 -0700

    Added EpisodesGrid component to Homepage

---
+++ src/components/EpisodesGrid.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import episodesService from '../services/episodesService';
+import EpisodeEntry from './EpisodeEntry';
+
+interface IEpisodeGridProps {
+    activeSeason: number;
+}
+
+export default function EpisodeGrid(props: IEpisodeGridProps) {
+    const { loading, error, episodes } = episodesService.useEpisodes();
+
+    return (
+        <div className='episodes-block'>
+            {episodes &&
+                episodes
+                    .filter(episode => episode.season_number === props.activeSeason)
+                    .map(episode => (
+                        <EpisodeEntry
+                            key={episode.id}
+                            id={episode.id}
+                            title={episode.title}
+                            description={episode.description}
+                            seasonNumber={episode.season_number}
+                            episodeNumber={episode.episode_number}
+                        />
+                    ))}
+        </div>
+    );
+}
--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import AppDescription from './AppDescription';
 import AppTitle from './AppTitle';
+import EpisodesGrid from './EpisodesGrid';
 import SeasonSelector from './SeasonSelector';
 
 export default function HomeScreen(): JSX.Element {
-    //const { loading, error, episodes } = episodesService.useEpisodes();
+    const [activeSeason, setActiveSeason] = useState(1);
 
     useEffect(() => {
         document.title = 'Rick And Morty Blog'; //Page title
@@ -15,6 +16,7 @@ export default function HomeScreen(): JSX.Element {
             <AppTitle />
             <AppDescription />
             <SeasonSelector onSeasonChanged={setActiveSeason} />
+            <EpisodesGrid activeSeason={activeSeason} />
         </div>
     );
 }
