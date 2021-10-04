6c35be0c102132227b40f79f7160f5611b8bd521
Joseph <alvizoj@uci.edu>
2021-10-03 22:50:21 -0700

    Added extra text

--- src/components/AppDescription.tsx
+++ src/components/AppDescription.tsx
@@ -5,6 +5,8 @@ export default function AppDescription(): JSX.Element {
     return (
         <div className='app-description'>
             <p>Select an episode below and leave a comment as to why it&apos;s your favorite!</p>
+            <br />
+            <p>Or simply select an episode to read comments about that particular one</p>
         </div>
     );
 }
